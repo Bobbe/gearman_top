352365ff81ec21cad57ff1f7573caa2618725e85
Dima <dmitriyknnv@gmail.com>
2013-06-11 18:59:18 +0300

    Update gearman_top.sh

--- gearman_top.sh
+++ gearman_top.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" == '' ] ;
 then
-        echo "run ./gearman-top.sh <IP> <N>, where N - numer of cycles"
+        echo "run ./gearman_top.sh <IP>"
         exit 1
 else
         watch -n 1 -d '(echo status ; sleep 0.1) | netcat '$1' 4730 | awk -f column.awk'
