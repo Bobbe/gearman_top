f2b616713834faa15430bbba676fb91da8366710
Stanislav Tsepeniuk <stanislav.tsepeniuk@corp.hitwe.com>
2016-10-24 19:14:42 +0300

    Fix awk file path

--- gearman_top.sh
+++ gearman_top.sh
@@ -16,5 +16,6 @@ then
         echo "run ./gearman_top.sh <IP>"
         exit 1
 else
-        watch -n 1 -d -t '(echo status ; sleep 0.1) | '${nc_cmd}' '$1' 4730 | awk -f column.awk'
+        DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+        watch -n 1 -d -t '(echo status ; sleep 0.1) | '${nc_cmd}' '$1' 4730 | awk -f '${DIR}'/column.awk'
 fi
